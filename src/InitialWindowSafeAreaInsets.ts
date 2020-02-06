b4805998ff7d7a726ad6a702ba211429325d826a
hyochan <dooboolab@gmail.com>
2020-02-06 17:11:03 +0900

    Attempt to resolve #48

--- src/InitialWindowSafeAreaInsets.ts
+++ src/InitialWindowSafeAreaInsets.ts
@@ -1,11 +1,11 @@
-import { UIManager } from 'react-native';
 import { EdgeInsets } from './SafeArea.types';
+import { UIManager } from 'react-native';
 
 const RNCSafeAreaViewConfig = UIManager.getViewManagerConfig(
   'RNCSafeAreaView',
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ) as any;
 
-export default (RNCSafeAreaViewConfig.Constants != null
+export default (RNCSafeAreaViewConfig?.Constants
   ? RNCSafeAreaViewConfig.Constants.initialWindowSafeAreaInsets
   : null) as EdgeInsets | null;
