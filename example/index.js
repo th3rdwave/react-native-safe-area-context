e2101c6e37664b8b1be5dc4510e3690f29b548d6
Janic Duplessis <janicduplessis@gmail.com>
2020-05-02 18:22:05 -0400

    Remove unused WindowChangeEvent, remove log

--- example/index.js
+++ example/index.js
@@ -2,6 +2,4 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-console.log('HEJADWDWD');
-
 AppRegistry.registerComponent(appName, () => App);
