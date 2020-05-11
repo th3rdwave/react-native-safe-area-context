e25bf577b510f83054d921d0845774e6740737fd
Janic Duplessis <janicduplessis@gmail.com>
2020-05-11 16:58:23 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @format
- */
-
 const typescriptEslintRecommended = require('@typescript-eslint/eslint-plugin/dist/configs/recommended.json');
 const typescriptEslintPrettier = require('eslint-config-prettier/@typescript-eslint');
 
