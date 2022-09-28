c4a3ad17c09b7c20887d05f51ea339811fe3e93c
Janic Duplessis <janicduplessis@gmail.com>
2022-09-28 08:30:55 -0400

    fix: don't set codegen configs for older rn cli versions

--- react-native.config.js
+++ react-native.config.js
@@ -1,14 +1,26 @@
+let supportsCodegenConfig = false;
+try {
+  const rnCliAndroidVersion =
+    require('@react-native-community/cli-platform-android/package.json').version;
+  const [major] = rnCliAndroidVersion.split('.');
+  supportsCodegenConfig = major >= 9;
+} catch (e) {
+  // ignore
+}
+
 module.exports = {
   dependency: {
     platforms: {
-      android: {
-        libraryName: 'safeareacontext',
-        componentDescriptors: [
-          'RNCSafeAreaProviderComponentDescriptor',
-          'RNCSafeAreaViewComponentDescriptor',
-        ],
-        cmakeListsPath: 'src/main/jni/CMakeLists.txt',
-      },
+      android: supportsCodegenConfig
+        ? {
+            libraryName: 'safeareacontext',
+            componentDescriptors: [
+              'RNCSafeAreaProviderComponentDescriptor',
+              'RNCSafeAreaViewComponentDescriptor',
+            ],
+            cmakeListsPath: 'src/main/jni/CMakeLists.txt',
+          }
+        : {},
       macos: null,
       windows: null,
     },
