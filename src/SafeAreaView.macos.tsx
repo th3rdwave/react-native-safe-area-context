eaec631539c3683499bd937847ee7b8af4c2073e
Arkady Magomedov <magom001@Arkadys-MacBook-Pro.local>
2023-06-21 15:50:28 +0200

    Cast View to the NativeSafeAreaViewInstance for .macos and .windows to get the correct type definitions

--- src/SafeAreaView.macos.tsx
+++ src/SafeAreaView.macos.tsx
@@ -1,3 +1,4 @@
 import { View } from 'react-native';
+import type { NativeSafeAreaViewInstance } from './SafeArea.types';
 
-export const SafeAreaView = View;
+export const SafeAreaView = View as unknown as NativeSafeAreaViewInstance;
