44b1f434327fca7287bdc7bdc8678a19e241d26d
Janic Duplessis <janicduplessis@gmail.com>
2024-06-28 17:00:07 -0400

    chore: update deps and rn 0.74

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,8 @@
 module.exports = {
-  extends: ['@react-native-community'],
+  extends: ['@react-native'],
   plugins: ['@react-native/eslint-plugin-specs'],
   rules: {
     'react-native/no-inline-styles': 'off',
     '@react-native/specs/react-native-modules': 'error',
   },
-  overrides: [
-    {
-      files: ['src/specs/**/*.js'],
-      rules: {
-        '@react-native/specs/react-native-modules': 'error',
-      },
-    },
-  ],
 };
