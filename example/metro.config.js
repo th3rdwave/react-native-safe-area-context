f7d463030aa1269eea0aacdeaefa458c05787e30
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-07-23 21:29:16 +0900

    Fix metro config to resolve react-native-safe-area-context

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,5 +1,14 @@
-const { makeMetroConfig } = require("@rnx-kit/metro-config");
+const path = require('node:path');
+const { makeMetroConfig } = require('@rnx-kit/metro-config');
+
 module.exports = makeMetroConfig({
+  watchFolders: [path.dirname(__dirname)],
+  resolver: {
+    resolverMainFields: ['main', 'react-native'],
+    extraNodeModules: {
+      'react-native-safe-area-context': path.dirname(__dirname),
+    },
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
