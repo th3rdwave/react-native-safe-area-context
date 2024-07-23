bda78200738ad99bf31cd766a4d328c99c1a6062
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-07-23 23:13:46 +0900

    Write macOS equivalents for all iOS code

--- ios/RNCSafeAreaUtils.h
+++ ios/RNCSafeAreaUtils.h
@@ -1,10 +1,22 @@
 #import <React/RCTDefines.h>
 #import <React/RCTView.h>
+#if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
+#elif TARGET_OS_OSX
+#import <AppKit/AppKit.h>
+#endif
 
 extern NSString *const RNCSafeAreaDidChange;
 
 RCT_EXTERN BOOL
+#if TARGET_OS_IPHONE
 UIEdgeInsetsEqualToEdgeInsetsWithThreshold(UIEdgeInsets insets1, UIEdgeInsets insets2, CGFloat threshold);
+#elif TARGET_OS_OSX
+NSEdgeInsetsEqualToEdgeInsetsWithThreshold(NSEdgeInsets insets1, NSEdgeInsets insets2, CGFloat threshold);
+#endif
 
+#if TARGET_OS_IPHONE
 RCT_EXTERN UIViewController *RNCParentViewController(UIView *view);
+#elif TARGET_OS_OSX
+RCT_EXTERN NSViewController *RNCParentViewController(NSView *view);
+#endif
