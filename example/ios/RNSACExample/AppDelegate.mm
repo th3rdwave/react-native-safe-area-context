66e521c921a83d55fd8264305276249bc516dd24
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-07-24 22:49:29 +0900

    Restore example to its original state

---
+++ example/ios/RNSACExample/AppDelegate.mm
@@ -0,0 +1,27 @@
+#import "AppDelegate.h"
+
+#import <React/RCTBundleURLProvider.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  self.moduleName = @"RNSACExample";
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self bundleURL];
+}
+
+- (NSURL *)bundleURL
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+@end
--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,24 +1,7 @@
-const project = (() => {
-  try {
-    const { configureProjects } = require("react-native-test-app");
-    return configureProjects({
-      android: {
-        sourceDir: "android",
-      },
-      ios: {
-        sourceDir: "ios",
-        automaticPodsInstallation: true,
-      },
-      windows: {
-        sourceDir: "windows",
-        solutionFile: "windows/SafeAreaContextExample.sln",
-      },
-    });
-  } catch (_) {
-    return undefined;
-  }
-})();
-
 module.exports = {
-  ...(project ? { project } : undefined),
+  project: {
+    ios: {
+      automaticPodsInstallation: true,
+    },
+  },
 };
