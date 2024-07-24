a20f19d9d3e1398dace1eee83629ddc5a7cfaa2f
shirakaba <14055146+shirakaba@users.noreply.github.com>
2024-07-24 22:49:29 +0900

    Restore example to its original state

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,20 +1,49 @@
-const path = require('node:path');
-const { makeMetroConfig } = require('@rnx-kit/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+const escape = require('escape-string-regexp');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const pak = require('../package.json');
 
-module.exports = makeMetroConfig({
-  watchFolders: [path.dirname(__dirname)],
+const root = path.resolve(__dirname, '..');
+
+const modules = Object.keys({
+  ...pak.peerDependencies,
+});
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  projectRoot: __dirname,
+  watchFolders: [root],
+
+  // We need to make sure that only one version is loaded for peerDependencies
+  // So we block them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    resolverMainFields: ['main', 'react-native'],
-    extraNodeModules: {
-      'react-native-safe-area-context': path.dirname(__dirname),
-    },
+    blacklistRE: exclusionList(
+      modules.map(
+        (m) =>
+          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
+      ),
+    ),
+
+    extraNodeModules: modules.reduce((acc, name) => {
+      acc[name] = path.join(__dirname, 'node_modules', name);
+      return acc;
+    }, {}),
   },
+
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
-});
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
