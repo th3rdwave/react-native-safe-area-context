2116a0e036530ca9743eb255b5dc18533cadfcd7
chrfalch <christian.falch@gmail.com>
2024-10-11 12:29:34 +0200

    Removed a check that is no longer needed
    We've had some issue with monorepos when resolving the codegen config on Android in other libraries, and found that due to versions no longer in use we could now remove the version test in `react-native-config.js` and always return the component descriptor etc.
    
    (The monorepo issue was with using `require.main.require` which will resolve to the calling script and not the module)
    
    For reference, here is the same PR in @react-native-community/slider:
    
    https://github.com/callstack/react-native-slider/pull/657

--- react-native.config.js
+++ react-native.config.js
@@ -1,27 +1,14 @@
-let supportsCodegenConfig = false;
-try {
-  const rnCliAndroidVersion = require.main.require(
-    '@react-native-community/cli-platform-android/package.json',
-  ).version;
-  const [major] = rnCliAndroidVersion.split('.');
-  supportsCodegenConfig = major >= 9;
-} catch (e) {
-  // ignore
-}
-
 module.exports = {
   dependency: {
     platforms: {
-      android: supportsCodegenConfig
-        ? {
-            libraryName: 'safeareacontext',
-            componentDescriptors: [
-              'RNCSafeAreaProviderComponentDescriptor',
-              'RNCSafeAreaViewComponentDescriptor',
-            ],
-            cmakeListsPath: 'src/main/jni/CMakeLists.txt',
-          }
-        : {},
+      android: {
+        libraryName: 'safeareacontext',
+        componentDescriptors: [
+          'RNCSafeAreaProviderComponentDescriptor',
+          'RNCSafeAreaViewComponentDescriptor',
+        ],
+        cmakeListsPath: 'src/main/jni/CMakeLists.txt',
+      },
       macos: null,
       windows: null,
     },
