b9434af6a805a2dd606226d6fcb28c11f4316c10
chrfalch <christian.falch@gmail.com>
2024-10-16 13:24:05 +0200

    cr: Reverted and simplified change after code review
    Removed previous change and made the `supportsCodegenConfig` variable true initially

--- react-native.config.js
+++ react-native.config.js
@@ -1,14 +1,27 @@
+let supportsCodegenConfig = true;
+try {
+  const rnCliAndroidVersion = require.main.require(
+    '@react-native-community/cli-platform-android/package.json',
+  ).version;
+  const [major] = rnCliAndroidVersion.split('.');
+  supportsCodegenConfig = major >= 9;
+} catch (e) {
+  // ignore
+}
+
 module.exports = {
   dependency: {
     platforms: {
-      android: {
-        libraryName: 'safeareacontext',
-        componentDescriptors: [
-          'RNCSafeAreaProviderComponentDescriptor',
-          'RNCSafeAreaViewComponentDescriptor',
-        ],
-        cmakeListsPath: 'src/main/jni/CMakeLists.txt',
-      },
+      android: supportsCodegenConfig
+        ? {
+            libraryName: 'safeareacontext',
+            componentDescriptors: [
+              'RNCSafeAreaProviderComponentDescriptor',
+              'RNCSafeAreaViewComponentDescriptor',
+            ],
+            cmakeListsPath: 'src/main/jni/CMakeLists.txt',
+          }
+        : {},
       macos: null,
       windows: null,
     },
