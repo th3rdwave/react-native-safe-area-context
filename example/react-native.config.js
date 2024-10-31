abe15139b641ff1c3c8f989df3c24821befe3384
Vojtech Novak <vonovak@gmail.com>
2024-10-31 21:35:00 +0100

    feat: support RN 0.76 + add example with RNTA (#538)
    * feat: init an example with react-native-test-app
    
    * feat: copy over sources
    
    * chore: delete old example
    
    * chore: rename rnta-example to example
    
    * chore: remove macos example
    
    * chore: remove patch
    
    * chore: update packages
    
    * chore: update CI

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,7 +1,24 @@
+const project = (() => {
+  try {
+    const { configureProjects } = require('react-native-test-app');
+    return configureProjects({
+      android: {
+        sourceDir: 'android',
+      },
+      ios: {
+        sourceDir: 'ios',
+        automaticPodsInstallation: true,
+      },
+      windows: {
+        sourceDir: 'windows',
+        solutionFile: 'windows/safe-area-example.sln',
+      },
+    });
+  } catch (_) {
+    return undefined;
+  }
+})();
+
 module.exports = {
-  project: {
-    ios: {
-      automaticPodsInstallation: true,
-    },
-  },
+  ...(project ? { project } : undefined),
 };
